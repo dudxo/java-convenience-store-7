eedd14ad4a5196ee296dbe32379aea99763253a6
dudxo <father8475@naver.com>
2024-11-07 17:08:06 +0900

    feat(PromotionType): 프로모션 타입 Enum - 프로모션 타입 관심사로 묶인 Enum을 선언한다. - 프로모션 상세 내용을 통해 Enum을 찾을 수 있다.

---
+++ src/main/java/domain/PromotionType.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum PromotionType {
+
+    NO_PROMOTION(false, ""),
+    CARBONATED_DRINK_BYE_TWO_GET_ONE(true, "탄산2+1"),
+    RECOMMENDED_PRODUCT(true, "md추천상품"),
+    FLASH_DISCOUNT(true, "반짝할인");
+
+
+    private final boolean isPromotion;
+    private final String promotionDetail;
+
+    PromotionType(boolean isPromotion, String promotionDetail) {
+        this.isPromotion = isPromotion;
+        this.promotionDetail = promotionDetail;
+    }
+
+    public static PromotionType of(String promotionDetail) {
+        return Arrays.stream(values())
+                .filter(promotionType -> promotionType.isEqual(promotionDetail))
+                .findFirst()
+                .orElse(NO_PROMOTION);
+    }
+
+    private boolean isEqual(String promotionDetail) {
+        return promotionDetail == this.promotionDetail;
+    }
+}
