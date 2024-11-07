dd6addb6b11f044b73ce2e302696f2c6a80ded34
dudxo <father8475@naver.com>
2024-11-07 18:01:25 +0900

    feat(Product): toString 오버라이딩
    - 상품 내용을 양식에 맞게 출력하기 위해 toString을 오버라이딩한다.
    - 상품 수량이 0개 일때 '재고 없음'이 출력 되도록한다.
    - 상품 가격은 1,000단위 ','형태를 포함하여 출력되도록 한다.

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -17,4 +17,25 @@ public class Product {
     private static PromotionType of(String promotion) {
         return PromotionType.of(promotion);
     }
+
+    @Override
+    public String toString() {
+        return this.name + " "
+                + formatPrice()
+                + formatQuantity()
+                + this.promotion.toString();
+    }
+
+    private String formatPrice() {
+        return String.format("%,d", this.price) + "원 ";
+    }
+
+    private String formatQuantity() {
+        final String ZERO_QUANTITY = "재고 없음";
+        if (this.quantity == 0) {
+            return ZERO_QUANTITY;
+        }
+        final String ONE_OR_THAN_QUANTITY = this.quantity + "개 ";
+        return ONE_OR_THAN_QUANTITY;
+    }
 }
