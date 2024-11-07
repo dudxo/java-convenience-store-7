245da160b5f3d44109bb19ad82fd300aa8404159
dudxo <father8475@naver.com>
2024-11-07 17:15:01 +0900

    feat(Stroage): 창고
    - 프로모션 재고와 일반 재고를 갖는 창고를 정의한다.

---
+++ src/main/java/domain/Storage.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public class Storage {
+
+    private final List<Product> products;
+
+    public Storage(List<Product> products) {
+        this.products = products;
+    }
+}
