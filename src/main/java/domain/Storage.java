e757ab41cd51fabbe87071fe8c4f9da016a3a4e0
dudxo <father8475@naver.com>
2024-11-07 20:07:32 +0900

    feat(Storage): toString 오버라이딩
    - 총 재고(상품 목록)을 보여주기 위해 toString을 재정의한다.

--- src/main/java/domain/Storage.java
+++ src/main/java/domain/Storage.java
@@ -9,4 +9,13 @@ public class Storage {
     public Storage(List<Item> items) {
         this.items = items;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Item item : items) {
+            sb.append("- ").append(item.toString()).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
 }
