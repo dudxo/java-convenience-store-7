625f9f39fb6b0454ca21f437f756e19ae178a27e
dudxo <father8475@naver.com>
2024-11-11 03:48:37 +0900

    feat(OutputView): 영수증 출력
    - 영수증 화면 구성을 출력한다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.Order;
+import domain.OrderItem;
 import domain.Storage;
 
 public class OutputView {
@@ -22,6 +24,49 @@ public class OutputView {
 
     public void printMembership() {
         ViewMessage.OUTPUT_MEMBERSHIP_MSG.print();
+    }
+
+    public void printActualPrice(Order order) {
+        ViewMessage.OUTPUT_ACTUAL_PRICE.print(order.getActualTotalPrice());
+    }
+
+    public void printTotalMembershipDiscount(Order order) {
+        ViewMessage.OUTPUT_MEMBERSHIP_DISCOUNT.print(-order.getMembershipDisCount());
+    }
+
+    public void printTotalPromotionDiscount(Order order) {
+        ViewMessage.OUTPUT_EVENT_DISCOUNT.print(-order.getTotalOrderPromotionDiscount());
+    }
+
+    public void printTotalPurchasePrice(Order order) {
+        ViewMessage.OUTPUT_TOTAL_PURCHASE_PRICE_MSG.print(order.getTotalOrderQuantity(), order.getTotalOrderPrice());
+    }
+
+    public void printGiftItemLine(int maxItemNameLength, OrderItem orderItem) {
+        ViewMessage.OUTPUT_GIFT_LINE.printGiftLineWithDynamicFormat(maxItemNameLength, orderItem.getName(),
+                orderItem.getPromotionQuantity());
+    }
+
+    public void printGiftHeader() {
+        ViewMessage.OUTPUT_GIFT_HEADER.print();
+    }
+
+    public void printPurchaseItemLine(int maxItemNameLength, OrderItem orderItem) {
+        ViewMessage.OUTPUT_ITEM_LINE.printItemLineWithDynamicFormat(maxItemNameLength, orderItem.getName(),
+                orderItem.getPurchaseQuantity(),
+                orderItem.getTotalPrice());
+    }
+
+    public void printReceiptHeader() {
+        ViewMessage.OUTPUT_RECEIPT_HEADER.print();
+    }
+
+    public void printReceiptTag(int maxItemNameLength, String name, String quantity, String price) {
+        ViewMessage.OUTPUT_RECEIPT_TAG.printItemLineWithDynamicFormat(maxItemNameLength, name, quantity, price);
+    }
+
+    public void printResultLine() {
+        ViewMessage.OUTPUT_RESULT_LINE.print();
     }
 
 
