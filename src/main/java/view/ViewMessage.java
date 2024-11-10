66c88637c073c79b8a8f3849e883341cce1068ef
dudxo <father8475@naver.com>
2024-11-11 05:15:50 +0900

    feat(ViewMessage): 줄 바꿈 추가

--- src/main/java/view/ViewMessage.java
+++ src/main/java/view/ViewMessage.java
@@ -6,7 +6,7 @@ public enum ViewMessage {
     INPUT_NOT_PROMOTION_DISCOUNT_MSG("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n"),
     INPUT_MEMBERSHIP_DISCOUNT_MSG("멤버십 할인을 받으시겠습니까? (Y/N)%n"),
     INPUT_ADDITION_PURCHASE_MSG("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)%n"),
-    OUTPUT_WELCOME_MSG("안녕하세요. W편의점입니다.%n현재 보유하고 있는 상품입니다.%n%n"),
+    OUTPUT_WELCOME_MSG("%n안녕하세요. W편의점입니다.%n현재 보유하고 있는 상품입니다.%n%n"),
     OUTPUT_STOCK_ITEMS_MSG("%s"),
     OUTPUT_GET_ADDITIONAL_GIFT_MSG("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n"),
     OUTPUT_NOT_PROMOTION_DISCOUNT_MSG("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n"),
