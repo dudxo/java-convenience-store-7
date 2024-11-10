b06e9e719db04423cc07766dbc54afaa67bdd2fc
dudxo <father8475@naver.com>
2024-11-11 03:51:03 +0900

    feat(Order): 구입 품목 최대 글자 수
    - 구입한 품목 이름 중 최대 글자수를 찾는다.

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -50,4 +50,11 @@ public class Order {
     public int getActualTotalPrice() {
         return getTotalOrderPrice() - getTotalOrderPromotionDiscount() - this.membershipDisCount;
     }
+
+    public int getMaxItemNameLength() {
+        return orderItems.stream()
+                .mapToInt(item -> item.getName().length())
+                .max()
+                .orElse(1);
+    }
 }
