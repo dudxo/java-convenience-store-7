e1ddf2ef42544ec29ffceff34a3c64e3709f7ab6
dudxo <father8475@naver.com>
2024-11-10 23:47:49 +0900

    feat(Stroage): 재고
    - 편의점에 존재하는 모든 상품을 관리하는 재고를 정의한다.

--- src/main/java/domain/Storage.java
+++ src/main/java/domain/Storage.java
@@ -18,4 +18,42 @@ public class Storage {
         }
         return sb.toString();
     }
+
+    public List<Item> getItems() {
+        return List.copyOf(items);
+    }
+
+    public boolean isPromotion(String itemName) {
+        return this.getItems().stream()
+                .filter(item -> item.containsName(itemName))
+                .anyMatch(Item::isPromotionItem);
+    }
+
+    public Item findPromotionItem(String itemName) {
+        return this.getItems().stream()
+                .filter(item -> item.containsName(itemName))
+                .filter(Item::isPromotionItem) // 프로모션 상품인지 확인
+                .findFirst()
+                .orElse(null); // 찾지 못하면 null 반환
+    }
+
+    public Item findGeneralItem(String itemName) {
+        return this.getItems().stream()
+                .filter(item -> item.containsName(itemName))
+                .filter(item -> !item.isPromotionItem())
+                .findFirst()
+                .orElse(null); // 찾지 못하면 null 반환
+    }
+
+    public boolean containsItem(String itemName) {
+        return items.stream()
+                .anyMatch(item -> item.containsName(itemName));
+    }
+
+    public boolean isEnoughQuantity(String itemName, int requiredQuantity) {
+        return this.getItems().stream()
+                .filter(item -> item.containsName(itemName))
+                .filter(item -> !item.isPromotionItem())
+                .anyMatch(item -> item.isEnoughQuantity(requiredQuantity));
+    }
 }
