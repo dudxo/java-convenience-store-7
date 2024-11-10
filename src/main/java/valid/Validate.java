3843a1a90c49f788d51a07e056c973b80980a0be
dudxo <father8475@naver.com>
2024-11-10 23:15:57 +0900

    feat(Valid): 검증
    - 안내 메시지에 대한 대답이 'Y' 또는 'N'인지 검증한다.
    - 구입할 상품과 수량 형식이 올바른지 검증한다.
    - 존재하는 상품명인지 검증한다.
    - 구매 수량이 재고 수량을 초과 했는지 검증한다.

---
+++ src/main/java/valid/Validate.java
@@ -0,0 +1,53 @@
+package valid;
+
+import domain.CartItem;
+import domain.Storage;
+import exception.ErrorMessage;
+
+public class Validate {
+
+    private static final String INPUT_PURCHASE_REGEX = "\\[([\\w가-힣]+)-(\\d+)\\]";
+
+    public static void validateAnswer(String answer) {
+        if (isYesAnswer(answer) || isNoAnswer(answer)) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_ANSWER_MSG.getMessage());
+    }
+
+    public static boolean isYesAnswer(String answer) {
+        return "Y".equalsIgnoreCase(answer);
+    }
+
+    public static boolean isNoAnswer(String answer) {
+        return "N".equalsIgnoreCase(answer);
+    }
+
+
+    public static void validateItem(String[] items) {
+        for (String item : items) {
+            if (isMatchItemRegex(item)) {
+                continue;
+            }
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_INPUT_MSG.getMessage());
+        }
+    }
+
+    private static boolean isMatchItemRegex(String item) {
+        return item.matches(INPUT_PURCHASE_REGEX);
+    }
+
+    public static void existItemNameInStorage(Storage storage, String itemName) {
+        if (storage.containsItem(itemName)) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.NOT_FOUND_ITEM_MSG.getMessage());
+    }
+
+    public static void validateEnoughStock(Storage storage, CartItem cartItem) {
+        if (storage.isEnoughQuantity(cartItem.getName(), cartItem.getQuantity())) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.EXCEED_STOCK_MSG.getMessage());
+    }
+}
