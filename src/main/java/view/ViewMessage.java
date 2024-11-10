e6f4dd4dad6b20af891ff1681d775fa3e7a6cd6b
dudxo <father8475@naver.com>
2024-11-11 03:49:23 +0900

    feat(ViewMessage): 출력 메시지
    - 영수증 출력 구성 메시지들을 선언한다.
    - 영수증 물품 부분과 증정품 부분의 동적 포맷팅을 적용한다.

--- src/main/java/view/ViewMessage.java
+++ src/main/java/view/ViewMessage.java
@@ -11,7 +11,17 @@ public enum ViewMessage {
     OUTPUT_GET_ADDITIONAL_GIFT_MSG("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n"),
     OUTPUT_NOT_PROMOTION_DISCOUNT_MSG("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n"),
     OUTPUT_MEMBERSHIP_MSG("멤버십 할인을 받으시겠습니까? (Y/N)%n"),
-    ;
+    OUTPUT_RECEIPT_HEADER("==============W 편의점================%n"),
+    OUTPUT_RECEIPT_TAG("%%-%ds\t%%10s\t%%10s%n"),
+
+    OUTPUT_ITEM_LINE("%%-%ds\t%%10s\t%%10s%n"),
+    OUTPUT_GIFT_HEADER("=============증    정================%n"),
+    OUTPUT_GIFT_LINE("%%-%ds\t%10d%n"),
+    OUTPUT_RESULT_LINE("====================================%n"),
+    OUTPUT_TOTAL_PURCHASE_PRICE_MSG("총구매액%10d\t%,10d%n"),
+    OUTPUT_EVENT_DISCOUNT("행사할인\t\t\t\t%,10d%n"),
+    OUTPUT_MEMBERSHIP_DISCOUNT("멤버십할인\t\t\t\t%,10d%n"),
+    OUTPUT_ACTUAL_PRICE("내실돈\t\t\t\t%,10d%n");
 
 
     private final String message;
@@ -26,6 +36,16 @@ public enum ViewMessage {
 
     public void print(Object... args) {
         System.out.printf(message, args);
+    }
+
+    public void printItemLineWithDynamicFormat(int maxItemNameLength, Object... args) {
+        String format = String.format("%%-%ds\t%%10s\t%%10s%n", maxItemNameLength);
+        System.out.printf(format, args);
+    }
+
+    public void printGiftLineWithDynamicFormat(int maxItemNameLength, Object... args) {
+        String format = String.format("%%-%ds\t%%10d%n", maxItemNameLength);  // 포맷을 동적으로 생성
+        System.out.printf(format, args);  // 생성된 포맷을 사용하여 출력
     }
 
 }
