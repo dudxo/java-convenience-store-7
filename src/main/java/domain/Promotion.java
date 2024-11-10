3600dd3bc21ef0df94131edc22264bdab9abb012
dudxo <father8475@naver.com>
2024-11-10 23:28:41 +0900

    feat(Promotion): 프로모션
    - 프로모션 타입에 따른 상세 정보(구매 수량, 증정 수량, 시작일, 종료일)을 정의한다.
    - 프로모션 타입이 같은지 확인한다.
    - 구매수량에 따른 총 증정 수량을 계산한다.
    - 프로모션 적용에 필요한 총 수량을 계산한다.
    - 프로모션 기간에 해당하는지 확인한다.
    - 프로모션 필요 조건이 충족되었는지 확인한다.

--- src/main/java/domain/Promotion.java
+++ src/main/java/domain/Promotion.java
@@ -1,16 +1,17 @@
 package domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Promotion {
 
     private final PromotionType promotionType;
     private final int purchaseAmount;
     private final int giftAmount;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
 
-    public Promotion(String promotion, int purchaseAmount, int giftAmount, LocalDate startDate, LocalDate endDate) {
+    public Promotion(String promotion, int purchaseAmount, int giftAmount, LocalDateTime startDate,
+                     LocalDateTime endDate) {
         this.promotionType = convertToPromotionType(promotion);
         this.purchaseAmount = purchaseAmount;
         this.giftAmount = giftAmount;
@@ -20,5 +21,82 @@ public class Promotion {
 
     private PromotionType convertToPromotionType(String promotion) {
         return PromotionType.of(promotion);
+    }
+
+    public boolean contains(String promotionDetail) {
+        if (this.promotionType.getPromotionDetail().equals(promotionDetail)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public PromotionType getPromotionType() {
+        return promotionType;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public int getGiftAmount() {
+        return giftAmount;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public int calculateActualPurchaseQuantity(int requestedQuantity) {
+        int totalSets = requestedQuantity / (purchaseAmount + giftAmount); // 10 /3 = 3
+        int remainder = requestedQuantity % (purchaseAmount + giftAmount);  // 10 % 3 = 1
+
+        return (totalSets * purchaseAmount) + remainder;  // 3 * 2 + 1
+    }
+
+    public int calculateRemainderQuantity(int requestedQuantity) {
+        int remainder = requestedQuantity % (purchaseAmount + giftAmount);
+
+        return remainder;  // 1
+    }
+
+    public int calculateActualPurchaseQuantity2(int requestedQuantity) {
+        int totalSets = requestedQuantity / (purchaseAmount + giftAmount);
+
+        return totalSets * purchaseAmount;  // 6
+    }
+
+    public int calculateGiftQuantity(int requestedQuantity) {
+        int totalSets = requestedQuantity / (purchaseAmount + giftAmount);
+
+        return totalSets * giftAmount;
+    }
+
+    public int getTotalPromotionQuantity() {
+        return this.purchaseAmount + this.giftAmount;
+    }
+
+    public boolean isCurrentPromotion(LocalDateTime today) {
+        return !today.isBefore(startDate) && !today.isAfter(endDate);
+    }
+
+    public boolean isMeetPromotionCondition(int requestedQuantity) {
+        if (isBuyPromotionQuantity(requestedQuantity) && isTotalQuantity(requestedQuantity)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBuyPromotionQuantity(int requestedQuantity) {
+        return this.purchaseAmount < requestedQuantity;
+    }
+
+    private boolean isTotalQuantity(int requestedQuantity) {
+        return requestedQuantity >= getTotalPromotionQuantity();
     }
 }
