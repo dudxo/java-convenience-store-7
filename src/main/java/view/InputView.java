393c3d105f212f2569bf0b3d7e2d91aa9f0bf142
dudxo <father8475@naver.com>
2024-11-11 20:45:24 +0900

    feat(InputView): inputAnswer
    - 답변을 입력받고 Y 또는 N인지 검증한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,13 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import valid.Task;
+import valid.Validate;
 
 public class InputView {
 
     public String inputItem() {
         return getInput(ViewMessage.INPUT_PURCHASE_ITEMS_MSG.getMessage());
-    }
-
-    public String inputAnswer() {
-        return Console.readLine().trim();
     }
 
     public String inputAdditionalPurchase() {
@@ -19,5 +17,13 @@ public class InputView {
     private String getInput(String message) {
         System.out.printf(message);
         return Console.readLine().trim();
+    }
+
+    public String inputAnswer() {
+        return Task.reTryTaskUntilSuccessful(() -> {
+            String answer = Console.readLine().trim();
+            Validate.validateAnswer(answer);
+            return answer;
+        });
     }
 }
